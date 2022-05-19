f9177a301cb436fab4dbffa3cb429080d6e5e07b
vincensiusadyatma <adyatmav@gmail.com>
2022-05-19 11:57:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["index.html"],
+  darkMode : 'class',
+  theme: {
+    extend: {
+      colors :{
+        'dark' : "#0f172a",
+        'abu-abu' :'#334155'
+      },
+      screens : {
+        '2xl' : '1320px',
+        '3xl'  : '1400px',
+        
+      },
+      fontFamily :{
+        'inter' : ['Inter']
+      }
+    },
+  },
+  plugins: [],
+}
